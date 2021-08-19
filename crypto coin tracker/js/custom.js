2e3d355ba177278dff3e80457d2f76cbd6b27a2d
Khit Juan <khitjuan2629@gmail.com>
2021-08-19 12:15:23 +0800

    Add files via upload
    added time update

--- crypto coin tracker/js/custom.js
+++ crypto coin tracker/js/custom.js
@@ -1,6 +1,38 @@
+var today = new Date();
+
 function initPage() {
-	var  months = ["JANUARY", "FEBRUARY", "MARCH", "APRIL", "MAY", "JUNE", "JULY", "AUGUST", "SEPTEMBER", "OCTOBER", "NOVEMBER", "DECEMBER"];
-	var today = new Date();
+	setDate();
+	setHour();
+}
+
+function setDate() {
+	var months = ["JANUARY", "FEBRUARY", "MARCH", "APRIL", "MAY", "JUNE", "JULY", "AUGUST", "SEPTEMBER", "OCTOBER", "NOVEMBER", "DECEMBER"];
 	var date = months[today.getMonth()]+' '+today.getDate()+', '+today.getFullYear();
 	document.getElementById("date_today").innerHTML = date;
+}
+
+function setHour() {
+	var hours = today.getHours();
+  	var minutes = today.getMinutes();
+	var ampm = "AM";
+	
+	if (hours >= 12) {
+		ampm = "PM";
+		
+		if (hours > 12) {
+			hours = hours - 12;
+		}
+	}
+	else {
+		ampm = "AM";
+		
+		if (hours = 0) {
+			hours = 12;
+		}
+	}
+	
+  	var strTime = hours + ":" + minutes + " " + ampm;
+	console.log(strTime);
+	document.getElementById("time_now").innerHTML = strTime;
+	document.title = "Crypto Price Checker: " + strTime;
 }
